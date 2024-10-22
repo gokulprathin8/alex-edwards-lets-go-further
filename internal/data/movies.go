0d511f4a2205f2e1caa2a8ef1e32e95c3b97b23c
Gokul Prathin <gokulprathin8@gmail.com>
2024-10-22 17:15:10 -0400

    better structuring

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Movie struct {
-	ID        int64
-	CreatedAt time.Time
-	Title     string
-	Year      int32
-	Runtime   int32
-	Genres    []string
-	Version   int32
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"-"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
 }
 
 func (m Movie) MarshalJSON() ([]byte, error) {
@@ -21,23 +21,17 @@ func (m Movie) MarshalJSON() ([]byte, error) {
 	var runtime string
 
 	if m.Runtime != 0 {
-		runtime = fmt.Sprintf("%d minutes", m.Runtime)
+		runtime = fmt.Sprintf("%d mins", m.Runtime)
 	}
 
+	type MovieAlias Movie
+
 	aux := struct {
-		ID      int64    `json:"id"`
-		Title   string   `json:"title"`
-		Year    int32    `json:"year,omitempty"`
-		Runtime string   `json:"runtime,omitempty"`
-		Genres  []string `json:"genres,omitempty"`
-		Version int32    `json:"version"`
+		MovieAlias
+		Runtime string `json:"runtime,omitempty"`
 	}{
-		ID:      m.ID,
-		Title:   m.Title,
-		Year:    m.Year,
-		Runtime: runtime,
-		Genres:  m.Genres,
-		Version: m.Version,
+		MovieAlias: MovieAlias(m),
+		Runtime:    runtime,
 	}
 
 	return json.Marshal(aux)
