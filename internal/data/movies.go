0d0164cc086014e1c1883589a3278ffee9658dc6
Gokul Prathin <gokulprathin8@gmail.com>
2024-10-22 17:10:35 -0400

    changes to movies and runtime custom json marshal

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,13 +1,44 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
 
 type Movie struct {
-	ID        int64     `json:"id,omitempty"`
-	CreatedAt time.Time `json:"-"`
-	Title     string    `json:"title,omitempty"`
-	Year      int32     `json:"year,omitempty"`
-	Runtime   Runtime   `json:"runtime,omitempty"`
-	Genres    []string  `json:"genres,omitempty"`
-	Version   int32     `json:"version,omitempty"`
+	ID        int64
+	CreatedAt time.Time
+	Title     string
+	Year      int32
+	Runtime   int32
+	Genres    []string
+	Version   int32
+}
+
+func (m Movie) MarshalJSON() ([]byte, error) {
+
+	var runtime string
+
+	if m.Runtime != 0 {
+		runtime = fmt.Sprintf("%d minutes", m.Runtime)
+	}
+
+	aux := struct {
+		ID      int64    `json:"id"`
+		Title   string   `json:"title"`
+		Year    int32    `json:"year,omitempty"`
+		Runtime string   `json:"runtime,omitempty"`
+		Genres  []string `json:"genres,omitempty"`
+		Version int32    `json:"version"`
+	}{
+		ID:      m.ID,
+		Title:   m.Title,
+		Year:    m.Year,
+		Runtime: runtime,
+		Genres:  m.Genres,
+		Version: m.Version,
+	}
+
+	return json.Marshal(aux)
 }
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -7,7 +7,7 @@ import (
 
 type Runtime int32
 
-func (r *Runtime) MarshalJSON() ([]byte, error) {
+func (r Runtime) MarshalJSON() ([]byte, error) {
 
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
